42cf6c71f5adfd3e702a1305d4d8f406df0928b1
MADHUPARNA PAN <107235930+honeyleaf20@users.noreply.github.com>
2023-07-05 00:11:33 +0530

    Add files via upload

---
+++ 042. Maximum Consecutive Ones.java
@@ -0,0 +1,15 @@
+042. Maximum Consecutive Ones
+
+public static int longestSubSeg(ArrayList<Integer> arr , int n, int k) {
+		// Write your code here.
+		int i=0,j=0;
+		for( i=0;i<n;i++){
+			if(arr.get(i)==0){
+				k--;
+			}
+			if(k<0 && arr.get(j++)==0){
+				k++;
+			}
+		}
+		return i-j;
+	}
