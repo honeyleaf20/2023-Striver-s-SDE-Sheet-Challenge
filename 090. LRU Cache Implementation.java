87289d824167171f28b590b906118821cb5d5f93
MADHUPARNA PAN <107235930+honeyleaf20@users.noreply.github.com>
2023-07-16 00:08:41 +0530

    Create 090. LRU Cache Implementation.java

---
+++ 090. LRU Cache Implementation.java
@@ -0,0 +1,51 @@
+public class LRUCache
+{
+    Node head = new Node(0,0);
+    Node tail = new Node(0,0);
+    HashMap<Integer,Node> hm = new HashMap<>();
+    int cap;
+    LRUCache(int capacity)
+    {
+        cap = capacity;
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key)
+    {
+        if(hm.containsKey(key)){
+            Node node = hm.get(key);
+            remove(node);
+            insert(node);
+            return node.value;
+        }
+        else{
+            return -1;
+        }
+    }
+
+    public void put(int key, int value)
+    {
+        if(hm.containsKey(key)){
+            remove(hm.get(key));
+        }
+        if(hm.size()==cap){
+            remove(tail.prev);
+        }
+        insert(new Node(key, value));
+    }
+    
+     public void insert(Node node) {
+        hm.put(node.key, node);
+        node.next = head.next;
+        node.next.prev = node;
+        head.next = node;
+        node.prev = head;
+     }
+    
+     public void remove(Node node) {
+        hm.remove(node.key);
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+}
